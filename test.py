aceee66c1543f610fa4c435bc00b9b2656a83776
Your Name <you@example.com>
2023-07-20 10:20:57 +0530

    test

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-print("hello world")
-print("push completed")
+def cousins(name):
+    print(name)
+
+cousins('theju')
+cousins('shreyu')
+cousins('deva')
+cousins('yuva')
+
+
+def greeting(greet,name):
+    print(f'{greet} {name}')
+    
+    
+greeting('hello', 'yuvetta')
